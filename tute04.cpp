2b6dcd7e0ca649d13bef35d741985a11ff54e5e5
it21048500 <87642831+IT21048500@users.noreply.github.com>
2021-07-19 09:57:28 +0000

    Tutorial 3 pdf done

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,23 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no){
+  long fac = 1;
+    for (int r=no; r >= 1; r--) {
+        fac = fac * r;
+  }
+  
+ 
+  return fac;
+}
+
+long nCr(int n, int r){
+  
+  long factN =Factorial(n) ;
+  long factR = Factorial(r);
+  long factSub = Factorial(n-r);
+
+  return factN / (factR *  factSub);
+  
+}
